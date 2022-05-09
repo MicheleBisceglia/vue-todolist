e1d63007269596a78dbde4530ea082767995295c
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-05-10 00:50:39 +0200

    style + vue

--- js/script.js
+++ js/script.js
@@ -1,30 +1,58 @@
+/*MILESTONE 1
+Stampare all'interno di una lista, un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato
+ viene letto e utilizzato per creare un nuovo todo,
+  che quindi viene aggiunto alla lista dei todo esistenti.*/
+
+//creo l'array
 const app = new Vue(
     {
         el:'#root',
         data: {
             todos: [
                 {
-                    text: 'fare la spesa',
+                    text: 'Fare esercizio pomeridiano',
                     done: false
                 },
                 {
-                    text: 'fare la spesa',
+                    text: 'Recap con Loris e Adriano',
                     done: false
                 },
                 {
-                    text: 'fare la spesa',
+                    text: 'Fare la spesa',
                     done: false
                 },
                 {
-                    text: 'fare la spesa',
-                    done: false
-                },
-                {
-                    text: 'fare la spesa',
+                    text: 'Anadare a prendere ilaria alla stazione',
                     done: false
                 }
-            ]
-        }
+            ],
+            newTodo: '',
+            
+        },
+        methods: {
+            toggleDone: function(index) {
+                const clickedTodo = this.todos[index];
+                clickedTodo.done = !clickedTodo.done;
+                    
+                },
+            addTodo: function() {
+                if(this.newTodo.length > 2) {
+                this.todos.push(this.newTodo);
+                this.newTodo = '';
+                }
+            },
+            removeTodo: function(index) {
+                console.log('remove',index);
+                this.todos.splice(index, 1)
+            },
+            
+            }
+    
     }
     
 )
