b0c4442a3a16dea67c664468a353dd326bdddb6a
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-05-09 17:09:23 +0200

    html/css structure

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const app = new Vue(
+    {
+        el:'#root',
+        data: {
+            todos: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                }
+            ]
+        }
+    }
+    
+)
